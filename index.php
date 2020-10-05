f789d56a920a1f033d43bf92b0313d961ef1708c
Manya <treham@rpi.edu>
2020-10-04 20:56:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  
   <!-- Master CSS -->
   <link rel="stylesheet" href="assets/master.css">
 </head>
@@ -21,17 +24,31 @@
     </div>
   </section>
 
-  <nav class="navbar nvarbar-expand-md navbar-dark bg-dark">
-    <ul class="nav nav-pills">
+  <nav class="navbar nvarbar-expand-md navbar-dark bg-dark sticky-top">
+  <ul class="nav nav-pills">
       <li class="nav-item">
-          <a class="nav-link active" href="#">Post</a>
+          <a class="nav-link active" href="explore.php">Explore</a>
       </li>
       <li class="nav-item">
-          <a class="nav-link" href="#">Explore</a>
-      </li>
+          <a class="nav-link" href="listings.php">Details</a>
+      </li>     
+      
       <li class="nav-item">
-          <a class="nav-link" href="#">Profile</a>
+          <a class="nav-link" href="addpost.php">Post</a>
       </li>
+ 
+      <!--Not Finished-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="dropdown-target">
+          Profile
+        <span class="caret"></span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="dropdown_target"> 
+        <a class="dropdown-item" href="profile.php">View Profile</a>
+        <a class="dropdown-item" href="profileedit.php">Edit Profile</a>
+            
+      </div>
+       </li> 
       <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Logout</a>
       </li>
