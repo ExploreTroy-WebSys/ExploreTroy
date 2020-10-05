92f29aec1d79275ff08e9969ce41d394684e9593
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-05 17:30:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
     </div>
     <div class="container columns">
       <div class="row explore-boxes justify-content-center">
-        <div class="shadow col-sm explore-box-1 explore-box">
+        <div class="shadow col-md-3 col-sm-12 explore-box-1 explore-box">
           <i class="fas fa-utensils fa-5x explore-icon"></i>          
         </div>
-        <div class="shadow col-sm explore-box-2 explore-box">
+        <div class="shadow col-md-3 col-sm-12 explore-box-2 explore-box">
           <i class="fas fa-store fa-5x explore-icon"></i>
         </div>
-        <div class="shadow col-sm explore-box-3 explore-box">
+        <div class="shadow col-md-3 col-sm-12 explore-box-3 explore-box">
           <i class="fas fa-hiking fa-5x explore-icon"></i>
         </div>
       </div>
