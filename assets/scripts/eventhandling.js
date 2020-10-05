bae127e6363f718453c062141f5845f7e78797b0
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-05 17:40:22 -0400

    Update eventhandling.js

--- assets/scripts/eventhandling.js
+++ assets/scripts/eventhandling.js
@@ -3,8 +3,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     var tron = document.getElementsByClassName("jumbotron");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    tron[0].style.padding = ".5%";
+    tron[0].style.paddingTop = ".5%";
   } else {
-    tron[0].style.padding = "5%";
+    tron[0].style.paddingTop = "5%";
   }
 }
