3aae177f80ce039aab92089c0f3085d1316c07be
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-10 12:38:45 -0400

    Update explore.php

---
+++ explore.php
@@ -0,0 +1,69 @@
+    <?php
+        session_start();
+        if (!(array_key_exists('authenticated', $_SESSION))) $_SESSION['authenticated'] = false;
+
+        # Include all boiler-plate head information for the site
+        include("assets/includes/head.php");
+    ?>
+<head>
+
+<body>
+    <?php 
+    # Include all boiler-plate header information for the site
+    include('assets/includes/header.php');
+    ?>
+    </header>
+
+    <main>
+        <section class="explore-page-main">
+        <div class="explore-category">
+            <h2>Explore [Category]</h2>
+        </div>
+        <div class="container-fluid explore-grid">
+            <div class="row justify-content-center">
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div><div class="col-sm-3 grid-item">
+                    hi
+                </div>
+                <div class="col-sm-3 grid-item">
+                    hi
+                </div>
+            </div>
+        </div>
+        </section>
+    </main>
+
+    <?php
+    include('assets/includes/footer.php');
+    # Include end of document boiler-plate for the site
+    include('assets/includes/foot.php');
+    ?>
+</body>
