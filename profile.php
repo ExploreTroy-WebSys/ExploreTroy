7a37c8a3d000619f2f80c8b47093dbd4f0f5a7d9
seanhale-rpi <hales3@rpi.edu>
2020-10-10 10:17:38 -0400

    Rudimentary Profile

---
+++ profile.php
@@ -0,0 +1,22 @@
+<?php 
+    # Spawn session cookie if one does not exist and set authentication status to false
+    session_start();
+    if (!(array_key_exists('authenticated', $_SESSION))) $_SESSION['authenticated'] = false;
+
+    # If the user isn't authenticated then redirect them to main
+    // if (!$_SESSION['authenticated']) header('location: index.php');
+
+    include_once("assets/includes/head.php");
+?>
+</head>
+<body onresize="repositionFooter()">
+    <?php include_once("assets/includes/header.php"); ?>
+    <main>
+        <h1>Test</h1>
+        <?php echo('<h1>Welcome ' . $_SESSION['rcsid'] . '!</h1>'); ?>
+    </main>
+
+    <?php
+        include_once("assets/includes/footer.php");
+        include_once("assets/includes/foot.php");
+?>
