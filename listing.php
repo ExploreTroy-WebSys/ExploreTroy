f9fd0879e36133062e348592c5db968aceaa6389
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-10-12 19:14:03 -0400

    Link attractions to listing page

---
+++ listing.php
@@ -0,0 +1,27 @@
+<?php
+        # Include all boiler-plate head information for the site
+        include("assets/includes/head.php");
+    ?>
+<head>
+
+<body>
+    <?php 
+    # Include all boiler-plate header information for the site
+    include('assets/includes/header.php');
+    ?>
+    </header>
+
+    <main>
+        <section class="explore-page-main">
+        <div class="explore-category">
+            <h2 class="explore-page-header-text">Reviews</h2>
+        </div>
+        </section>
+    </main>
+
+    <?php
+    include('assets/includes/footer.php');
+    # Include end of document boiler-plate for the site
+    include('assets/includes/foot.php');
+    ?>
+</body>
