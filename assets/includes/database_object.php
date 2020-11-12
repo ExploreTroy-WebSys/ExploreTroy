59f800669743532ae1162f626066942e2a480d7c
seanhale-rpi <hales3@rpi.edu>
2020-11-12 18:06:30 -0500

    Add the getQuery function

--- assets/includes/database_object.php
+++ assets/includes/database_object.php
@@ -11,4 +11,34 @@ if ($db->connect_errno) {
     die("Unable to reach ExploreTroy database: " . $db->connect_errno . ", " . $db->connect_error);
 }
 
+// Makes a user-defined query and returns a JSON interpretation of the reply entity
+function getQuery($query, $db_obj) {
+    // Array which will be JSON encoded
+    $output = array();
+
+    // Make the query to the DB
+    $query = $db_obj->query($query);
+
+    // Iterate over the response
+    while ($row = $query->fetch_array()) {
+        $tmp_out = array();
+
+        // Iterate over element in a given row response
+        foreach ($row as $key => $val) {
+            if (!is_numeric($key)) {
+                $tmp_out[$key] = $val;
+            }
+        }
+        $output[] = $tmp_out;
+    }
+
+    // If nothing was returned, return false
+    if (empty($output)) {
+        return false;
+    }
+
+    // Return the JSON interpretation of the user information
+    return json_encode($output);
+}
+
 ?>
