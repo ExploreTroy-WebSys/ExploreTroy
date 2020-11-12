e2c9fff933ad46579dfff4c714f383c4d87b45f3
Manya <treham@rpi.edu>
2020-11-11 19:23:04 -0500

    Updated Post Page

--- addpost.php
+++ addpost.php
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="/resources/demos/style.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body onresize="repositionFooter()">
   <?php
@@ -27,42 +28,46 @@
         <!-- Form header -->
         <section class="form-group">
           <section class="intro">
-          <p>Your first-hand experiences, recommendations, and questions truly help other travelers. Thanks!
+          <p><b>Your first-hand experiences, recommendations, and questions truly help other travelers. Thanks! </b>
           <br>
         </section>
         <section class="postpageborder">
+        <section class="info">
          <!-- Category selection -->
          <section class="options">
-            <p>Select the Type of Location: </p>
-            <input type="checkbox" id="restaurant" name="restaurant" value="Restaurant">
+            <p>Select category type: </p>
+            <input type="checkbox" id="restaurant" class="class" class="check" name="restaurant" value="Restaurant">
             <label for="restaurant">Restaurant</label><br>
             <div id="restaurant_items">
             <fieldset class="form-row justify-content-left chips">
                 <div class="col-md-auto chip">Fine-Dining</div>
                 <div class="col-md-auto chip">Fast food</div>
-                <div class="col-md-auto chip">Outdoor Dining</div>
+                <div class="col-md-auto chip">Outdoor Dining Available</div>
                 <label for="cars">Cuisine:</label>
                 <select name="cuisine" id="cuisine">
-                  <option value="volvo">Italian</option>
-                  <option value="saab">American</option>
-                  <option value="opel">Indian</option>
-                  <option value="audi">Thai</option>
+                <option value="" disabled selected hidden>Select One</option>
+                  <option value="italian">Italian</option>
+                  <option value="american">American</option>
+                  <option value="indian">Indian</option>
+                  <option value="thai">Thai</option>
+                  <option value="mexican">Mexican</option>
+                  <option value="thai">Chinese</option>
                 </select>
             </fieldset>
             </div>
             
-            <input type="checkbox" id="shop" name="shop" value="shop">
+            <input type="checkbox" id="shop" class="check" name="shop" value="shop">
             <label for="shop">Shop</label><br>
             <div id="shop_items">
             <fieldset class="form-row justify-content-left chips">
                 <div class="col-md-auto chip">Grocery</div>
                 <div class="col-md-auto chip">Clothing</div>
-                <div class="col-md-auto chip">Toy</div>
+                <div class="col-md-auto chip">Toys</div>
                 <div class="col-md-auto chip">Pharmacy</div>
               </fieldset>
               </div>
             
-            <input type="checkbox" id="excursion" name="excursion" value="excursion">
+            <input type="checkbox" id="excursion" class="check" name="excursion" value="excursion">
             <label for="Excursion">Excursion</label><br><br>
             <div id="excursion_items">
               <fieldset class="form-row justify-content-left chips">
@@ -75,7 +80,16 @@
             </div>
           </section>
 
-      <script>
+          <!-- Confirms that only one checkbox can be checked at one time-->
+          <script>
+          $(document).ready(function(){
+              $('input:checkbox').click(function() {
+              $('input:checkbox').not(this).prop('checked', false);
+              });
+          });
+          </script>
+
+          <script>
         document.getElementById('restaurant').onclick = function() {
         toggleSub(this, 'restaurant_items');
         };
@@ -86,7 +100,8 @@
             } else {
                 food.style.display = 'none';
             }
-        }
+        }  
+
         document.getElementById('shop').onclick = function() {
         toggleSub(this, 'shop_items');
         };
@@ -113,13 +128,13 @@
 
         <!-- Title of post and review body -->
         <section class="font">
-          <label for="exampleFormControlInput1">Title of Post</label>
+          <label for="exampleFormControlInput1">Title of post</label>
           <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Summarize your review or question">
           
           <br>
 
           <section class="form-group1">
-            <label for="exampleFormControlTextarea1">What's on your mind? Write a question or review.</label>
+            <label for="exampleFormControlTextarea1">What's on your mind? Ask a question or write a review.</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 
             <br>
@@ -137,10 +152,11 @@
           $( "#datepicker" ).datepicker();
           } );
           </script>
-          <p>Date of Vist: <input type="text" id="datepicker"></p>
+          <p>Date of visit: <input type="text" id="datepicker"></p>
           <br>
 
           <button type="submit" class="btn btn-primary">Submit your post!</button>
+        </section>
         </section>
       </form>
     </section>
