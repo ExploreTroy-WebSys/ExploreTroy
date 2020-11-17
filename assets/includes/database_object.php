29d85ab7319cfff69c4c8c1e4a774011bc850d14
seanhale-rpi <hales3@rpi.edu>
2020-11-17 16:43:54 -0500

    Restructure database interactions

--- assets/includes/database_object.php
+++ assets/includes/database_object.php
@@ -1,51 +1,105 @@
 <?php
 
-// Database credentials
-$db_hostname = "localhost";
-$db_username = "websys_user";
-$db_password = "websys_password";
-$db_name = "websys_final";
+class Database {
+    public $db;
 
-$db = new mysqli($db_hostname, $db_username, $db_password, $db_name);
-if ($db->connect_errno) {
-    die("Unable to reach ExploreTroy database: " . $db->connect_errno . ", " . $db->connect_error);
-}
-
-// Makes a user-defined query and returns a JSON interpretation of the reply entity
-function getQuery($query, $db_obj) {
-    // Array which will be JSON encoded
-    $output = array();
-
-    // Make the query to the DB
-    $query = $db_obj->query($query);
-
-    // Iterate over the response
-    while ($row = $query->fetch_array()) {
-        $tmp_out = array();
-
-        // Iterate over element in a given row response
-        foreach ($row as $key => $val) {
-            if (!is_numeric($key)) {
-                $tmp_out[$key] = $val;
-            }
+    public function __construct($db_t_hostname="mysql:host=localhost;dbname=websys_final", $db_username="websys_user", $db_password="websys_password") {
+        try {
+            $this->db = new PDO($db_t_hostname, $db_username, $db_password);
+        } catch(PDOException $e) {
+            throw new Exception('Database Connection Error: ' . $e->getMessage());
         }
-        $output[] = $tmp_out;
     }
 
-    // If nothing was returned, return false
-    if (empty($output)) {
-        return false;
+    public function getQuery($query, $param_array=null) {
+        // Array which will be JSON encoded
+        $output = array();
+    
+        try {
+            $query = $this->db->prepare($query);
+            $queryRes = $query->execute($param_array);
+            $query = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e) {
+            echo $e;
+        }
+    
+        foreach ($query as $row) {
+            $tmp_out = array();
+        
+            // Iterate over element in a given row response
+            foreach ($row as $key => $val) {
+                if (!is_numeric($key)) {
+                    $tmp_out[$key] = $val;
+                }
+            }
+            $output[] = $tmp_out;
+        }
+    
+        // If nothing was returned, return false
+        if (empty($output)) {
+            return false;
+        }
+    
+        // Return the JSON interpretation of the user information
+        return json_encode($output);
     }
 
-    // Return the JSON interpretation of the user information
-    return json_encode($output);
+    public function postQuery($query, $param_array=null) {
+        try {
+            $query = $this->db->prepare($query);
+            $queryRes = $query->execute($param_array);
+            return true;
+        }
+        catch(PDOException $e) {
+            return false;
+        }
+    }
 }
 
-function postQuery($query, $db_obj) {
-    if ($db_obj->query($query) == true) {
-        return true;
-    }
-    return false;
-}
+// function getQuery($query, $db_obj, $param_array=null) {
+//     // Array which will be JSON encoded
+//     $output = array();
+
+//     try {
+//         $query = $db_obj->prepare($query);
+//         $queryRes = $query->execute($param_array);
+//         $query = $query->fetchAll(PDO::FETCH_ASSOC);
+//     }
+//     catch(PDOException $e) {
+//         echo $e;
+//     }
+
+//     foreach ($query as $row) {
+//         $tmp_out = array();
+    
+//         // Iterate over element in a given row response
+//         foreach ($row as $key => $val) {
+//             if (!is_numeric($key)) {
+//                 $tmp_out[$key] = $val;
+//             }
+//         }
+//         $output[] = $tmp_out;
+//     }
+
+//     // If nothing was returned, return false
+//     if (empty($output)) {
+//         return false;
+//     }
+
+//     // Return the JSON interpretation of the user information
+//     return json_encode($output);
+// }
+
+// function postQuery($query, $db_obj, $param_array) {
+//     try {
+//         $query = $db_obj->prepare($query);
+//         $queryRes = $query->execute($param_array);
+//         return true;
+//     }
+//     catch(PDOException $e) {
+//         return false;
+//     }
+// }
 
 ?>
