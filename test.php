16d6c3367f5af4d15f8b7c33603e2bda796fa6c8
seanhale-rpi <hales3@rpi.edu>
2020-11-17 16:43:58 -0500

    More testing

--- test.php
+++ test.php
@@ -2,8 +2,13 @@
 
 include('assets/includes/database_object.php');
 
-$query = "SELECT * FROM `attractions`";
+$db = new Database();
 
-echo getQuery($query, $db);
+$query = "SELECT * FROM `attractions` WHERE `name` = :attrcName";
+// $query = "SELECT * FROM `attractions`";
+$param_array = array();
+$param_array[':attrcName'] = 'test place';
+
+echo $db->getQuery($query, $param_array);
 
 ?>
