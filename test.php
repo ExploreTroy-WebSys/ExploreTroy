8e2882c991d4d51269389e373b9a52a09dec5ec7
seanhale-rpi <hales3@rpi.edu>
2020-11-17 16:52:29 -0500

    Testing new postQuery()

--- test.php
+++ test.php
@@ -11,4 +11,16 @@ $param_array[':attrcName'] = 'test place';
 
 echo $db->getQuery($query, $param_array);
 
+$param_array = array();
+$param_array[':attrName'] = 'test place 2';
+$param_array[':attrDesc'] = 'with another test description';
+$param_array[':attrPhone'] = '987654321';
+$param_array[':attrRating'] = '4.1';
+$param_array[':attrAddr'] = '321 Loc Str. Troy, NY';
+$query = "INSERT INTO `attractions` (`name`, `description`, `phone`, `avg_rating`, `address`) VALU (:attrName, :attrDesc, :attrPhone, :attrRating, :attrAddr)";
+
+$res = $db->postQuery($query, $param_array);
+if ($res) echo "yay";
+else echo "oh no";
+
 ?>
