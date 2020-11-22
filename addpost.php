586543e3a44817148bacc97d8f2a1e4f46eb7c4e
Bonada <55992910+Bonada@users.noreply.github.com>
2020-11-22 10:46:49 -0500

    include closing tags and styling in head

--- addpost.php
+++ addpost.php
@@ -6,12 +6,7 @@
   # Include all boiler-plate head information for the site
   include("assets/includes/head.php");
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-</head>
+
 <body onresize="repositionFooter()">
   <?php
     # Include all boiler-plate header information for the site
