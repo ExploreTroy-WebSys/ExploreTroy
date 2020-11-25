bed25f0088f37399a3117a7eae7353c6840aec20
Manya <treham@rpi.edu>
2020-11-25 11:17:01 -0500

    Star Rating

--- addpostcurrent.php
+++ addpostcurrent.php
@@ -42,11 +42,62 @@
           </section>
 
           <section class="form-group1">
-            <label for="exampleFormControlTextarea1"><b>Rating </b></label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" name="rating" rows="3"></textarea>
+            <!-- <label for="exampleFormControlTextarea1"><b>Rating </b></label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" name="rating" rows="3"></textarea> -->
 
-            <br>
-        
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="star-rating">
+                <section class="font">
+                <div id="size">
+                <p><b>Select a rating </b> </p>
+            </div>
+                <span class="fa fa-star-o" data-rating="1"></span>
+                <span class="fa fa-star-o" data-rating="2"></span>
+                <span class="fa fa-star-o" data-rating="3"></span>
+                <span class="fa fa-star-o" data-rating="4"></span>
+                <span class="fa fa-star-o" data-rating="5"></span>
+                <input type="hidden" name="rating" class="rating-value">
+              </div>
+            </div>
+          </div>
+</section>
+ 
+          <script>
+          var $star_rating = $('.star-rating .fa');
+
+          var SetRatingStar = function() {
+            return $star_rating.each(function() {
+              if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+                return $(this).removeClass('fa-star-o').addClass('fa-star');
+              } else {
+                return $(this).removeClass('fa-star').addClass('fa-star-o');
+              }
+            });
+          };
+
+          $star_rating.on('click', function() {
+            $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+            return SetRatingStar();
+          });
+
+          SetRatingStar();
+          $(document).ready(function() {
+
+          });
+
+          $(fa-star).addClass('active'); 
+
+          var numStars = $(fa-star).parentsUntil("div").length+1;
+          $.post( "insert.php", { rating: numStars})
+              .done(function( data ) {
+                    $('.show-result').text(numStars + (numStars == 1 ? " star" : " stars!"));
+          });
+          </script>
+
+
+        <br>
           <!-- Date of visit -->
           <script>
           $( function() {
@@ -56,7 +107,7 @@
           <p><b>Date of visit:</b> <input type="text" name="date" id="datepicker"></p>
           <br>
 
-          <button type="submit" class="btn btn-primary">Submit your post!</button>
+        <button type="submit" class="btn btn-primary">Submit your post!</button>
         </section>
       </form>
     </section>
@@ -67,6 +118,7 @@
     # Include end of document boiler-plate for the site
     include('assets/includes/foot.php');
   ?>
+
 </body>
 </section>
 
