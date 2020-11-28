e1edfba8651a9a6164a38cd7f82bc5f590d2ba72
jssunray34 <55996087+jssunray34@users.noreply.github.com>
2020-11-28 16:25:12 -0500

    Float dropdowns to left

--- explore.php
+++ explore.php
@@ -51,7 +51,7 @@
     <main>
         <section class="explore-page-main">
             <div class="explore-category">
-                <h2 class="explore-page-header-text">Explore<?php if ($category != NULL) echo "&nbsp" . ucwords($category); ?></h2>
+                <h2 class="explore-page-header-text">Explore<?php if ($category != NULL) echo "&nbsp" . ucwords($category); if ($category == "restaurant") echo "s" ?></h2>
 
                 <div id="dropdown-menus">
                 <?php
