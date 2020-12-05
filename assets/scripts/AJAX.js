fe9f2122ffcca904c15b5ba7c1018445be128030
Bonada <55992910+Bonada@users.noreply.github.com>
2020-12-05 12:03:08 -0500

    Revert "Revert "admin""
    This reverts commit a216ae505baf4e0d09cce1558773cebed8bfdd33.

--- assets/scripts/AJAX.js
+++ assets/scripts/AJAX.js
@@ -125,6 +125,15 @@ function favoriteAttraction(attr_id) {
 }
 
 
+function deleteAttraction(attr_id) {
+    $.ajax({
+        type: "POST",
+        url: "backend/API/deleteAttraction.php",
+        async: false,
+        data: {"attrid": attr_id},
+    });
+}
+
 function followUser(follower, followed) {
     $.ajax({
         type: "POST",
