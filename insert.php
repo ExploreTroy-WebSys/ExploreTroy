2df4a2c9b20b38adce5830e13e0843a29e06789c
Manya <treham@rpi.edu>
2020-12-08 19:41:59 -0500

    Added Comments

--- insert.php
+++ insert.php
@@ -11,8 +11,6 @@ $review_body=$_POST['review_body'];
 $date=$_POST['date'];
 $rating=$_POST['rating'];
 
-
-
 $db = new Database();
 
 $host="localhost";
@@ -24,20 +22,20 @@ $conn=new mysqli($host,$db_username,$db_password,$dbname);
 // Get query string from url
 $attraction_id = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
 
-
+//Get user id of the signed in user
 $rcsid = $_SESSION['rcsid'];
 $query = "SELECT id FROM users WHERE rcsid = '" . $rcsid . "'";
 $result = $db->getQuery($query);
 $author_id = json_decode($result, true);
 $author_id = $author_id[0]['id'];
 
-
+//Insert review information into database
 if ($conn->connect_error){
     die('Connection Failed :'.$conn->connect_error);
 }else{
     $stmt=$conn->prepare("insert into reviews(author_id, attraction_id, title, review_body, date,rating)
     values(?,?,?,?,?,?)");
-    $stmt->bind_param('iisssi',$author_id, $attraction_id, $title, $review_body, $date, $rating,);
+    $stmt->bind_param('iisssi',$author_id, $attraction_id, $title, $review_body, $date, $rating);
     $stmt->execute();
     echo "Post Successfully Submitted";
     $stmt->close();
